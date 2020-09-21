b4af22faa8411de7a1a722f63125bc4a2f92413c
Renan Zamonelli <47351828+RZamonelli@users.noreply.github.com>
2020-09-21 12:14:09 -0300

    first

---
+++ TranscriptionState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StateKata
+{
+
+    public interface ITranscriptionState
+    {
+        string GetStateDocumentByCode(int stateCode);
+    }
+    public class TranscriptionState : ITranscriptionState
+    {
+        public string GetStateDocumentByCode(int stateCode)
+        {
+            string state = string.Empty;
+
+            switch (stateCode)
+            {
+                case 1:
+                    state = "Draft";
+                    break;
+                case 2:
+                    state = "Review";
+                    break;
+                case 3:
+                    state = "Approve";
+                    break;
+                case 4:
+                    state = "Published";
+                    break;
+            }
+
+            return state;
+        }
+    }
+}
